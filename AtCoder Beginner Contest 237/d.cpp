750e15b8857fb52ea94867d98479531b20dcacb3
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-03 08:06:55 +0800

    Create d.cpp

---
+++ AtCoder Beginner Contest 237/d.cpp
@@ -0,0 +1,25 @@
+/*
+[Q]https://atcoder.jp/contests/abc237/tasks/abc237_d
+*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+using namespace std;
+#define ulli unsigned long long int
+#define lli long long int
+int main(){
+	return 0;
+}
+/*
+[I1]
+5
+LRRLR
+[O1]
+1 2 4 5 3 0
+[I2]
+7
+LLLLLLL
+[O2]
+7 6 5 4 3 2 1 0
+*/
