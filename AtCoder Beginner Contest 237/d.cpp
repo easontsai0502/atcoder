60018b4c469f31f78763d94cff7f124828e8e365
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-03 08:23:03 +0800

    Update d.cpp

--- AtCoder Beginner Contest 237/d.cpp
+++ AtCoder Beginner Contest 237/d.cpp
@@ -41,3 +41,52 @@ LLLLLLL
 [O2]
 7 6 5 4 3 2 1 0
 */
+
+
+
+-----------------
+	/*
+[Q]https://atcoder.jp/contests/abc237/tasks/abc237_d
+*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<string>
+using namespace std;
+#define ulli unsigned long long int
+#define lli long long int
+int main(){
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+	vector<lli> vec;
+	vec.push_back(0);
+	vector<lli>::iterator vec_it=vec.begin();
+	for(int i=1;i<=n;i++){
+	    //*debug*/cout<<*vec_it;
+		if(s[i]=='R'){
+		    vec_it++;
+			vec_it=vec.insert(vec_it,i);
+		}else{
+			vec_it=vec.insert(vec_it,i);
+		}
+	}
+	for(int i=0;i<n;i++){
+		cout<<vec[i]<<" ";
+	}
+	return 0;
+}
+/*
+[I1]
+5
+LRRLR
+[O1]
+1 2 4 5 3 0
+[I2]
+7
+LLLLLLL
+[O2]
+7 6 5 4 3 2 1 0
+*/
