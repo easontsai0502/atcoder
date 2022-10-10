08691f3c15494ec990c5cb10d04eb2bb21626659
ET01 <EasonTsai0502@gmail.com>
2022-10-10 10:48:18 +0800

    a

--- dp/F.cpp
+++ dp/F.cpp
@@ -28,7 +28,7 @@ using namespace std;
 #define UINT unsigned INT
 #define PII pair<INT,INT>
 #define PUIUI pair<UINT,UINT>
-
+#define endl "\n"
 /*struct*/
 
 /*fn宣告*/
@@ -40,11 +40,14 @@ INT dp[3001][3001];
 INT lcs(INT n,INT m){
 	for(INT i=1;i<=n;i++){
 		for(INT j=1;j<=m;j++){
-			if(s[i-1]==t[j-1]){
+			if(s[i]==t[j]){
 				dp[i][j]=dp[i-1][j-1]+1;
 			}else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+			cout<<dp[i][j]<<",";
 		}
+		cout<<"\n";
 	}
+	return 0;
 }
 string backlcs(INT n,INT m){
 	string re="";
@@ -55,10 +58,11 @@ string backlcs(INT n,INT m){
 			i--;
 			j--;
 		}else{
-			if(dp[i][j]==dp[i-1][j]){
+			if(dp[i-1][j]>=dp[i][j-1]){
 				i--;
 			}else j--;
 		}
+		cout<<i<<","<<j<<" ";
 	}
 	reverse(re.begin(),re.end());
 	return re;
@@ -66,41 +70,16 @@ string backlcs(INT n,INT m){
 /*main*/
 int main(){
 	/*IO加速*/
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
 	/*CIN*/
 	string s,t;
 	cin>>s>>t;
 	/*solve*/
-	INT ss=s.size(),ts=t.size();
-	string dp[ss+1][ts+1];
-	for(INT i=0;i<=ss;i++){
-		for(INT j=0;j<=ts;j++){
-			//cout<<i<<","<<j<<" ";
-			dp[i][j]={NULL};
-		}
-		//cout<<"\n";
-	}
-	for(INT i=1;i<=ss;i++){
-		for(INT j=1;j<=ts;j++){
-			if(s[i-1]==t[j-1]){
-				dp[i][j]=dp[i-1][j-1];
-				dp[i][j].push_back(s[i-1]);
-			}else if(dp[i-1][j].size()>dp[i][j-1].size()){
-				dp[i][j]=dp[i-1][j];
-			}else{
-				dp[i][j]=dp[i][j-1];
-			}
-
-			//cout<<dp[i][j]<<",";
-		}
-		//cout<<"\n";
-	}
-	//for(int i=0;i<dp[ss][ts].size();i++){
-	//	cout<<dp[ss][ts][i];
-	//}
-	cout<<dp[ss][ts]<<"\n";
+	INT ss=s.length(),ts=t.length();
+	s=" "+s;
+	t=" "+t;
+	lcs(ss,ts);
+	cout<<backlcs(ss,ts);
 	return 0;
 }
 
